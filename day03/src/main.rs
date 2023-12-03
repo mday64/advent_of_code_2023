f0fb0cbec0ea89afbb04a9ea9e73f9780edea36b
Mark Day <mday@mac.com>
2023-12-03 15:04:27 -0700

    Day 3, part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -6,6 +6,10 @@ fn main() {
     let result1 = part1(input);
     println!("Part 1: {result1}");
     assert_eq!(result1, 559667);
+
+    let result2 = part2(input);
+    println!("Part 2: {result2}");
+    // assert_eq!(result2, 559667);
 }
 
 fn part1(input: &str) -> u32 {
@@ -52,8 +56,6 @@ fn part1(input: &str) -> u32 {
 
                     if has_adjacent_symbol {
                         total += current_number;
-                    } else {
-                        dbg!(current_number);
                     }
                 }
                 start_col = None;
@@ -62,6 +64,60 @@ fn part1(input: &str) -> u32 {
     }
 
     total
+}
+
+fn part2(input: &str) -> u32 {
+    // Find the location of all '*' symbols.  The value in the map is the
+    // list of adjacent numbers.
+    let mut gears = HashMap::new();
+    for (row, line) in input.lines().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch == '*' {
+                gears.insert((row as i32, col as i32), vec![]);
+            }
+        }
+    }
+
+    let mut current_number = 0;
+
+    // Find every number, including its starting and ending position.
+    for (row, line) in input.lines().enumerate() {
+        let mut start_col = None;
+        // We need to be sure to handle numbers at the end of a line.
+        // The easiest way is to pretend there is an extra '.' at the end
+        // of every line.
+        for (col, ch) in line.chars().chain(Some('.')).enumerate() {
+            if let Some(digit) = ch.to_digit(10) {
+                    if start_col.is_none() {
+                        start_col = Some(col as i32);
+                        current_number = 0;
+                    }
+                    current_number = current_number * 10 + digit;
+            } else {
+                if start_col.is_some() {
+                    // Finished parsing a number.  Look for adjacent '*' symbols.
+                    let row = row as i32;
+                    let start_col = start_col.unwrap();
+                    let end_col = col as i32;
+                    for r in row-1 ..= row+1 {
+                        for c in start_col-1 ..= end_col {
+                            // Append this number to this '*' symbol.
+                            if let Some(gear) = gears.get_mut(&(r,c)) {
+                                gear.push(current_number);
+                            }
+                        }
+                    }
+                }
+                start_col = None;
+            }
+        }
+    }
+
+    // Sum the products of all '*' symbols with exactly two numbers.
+    gears.values()
+    .filter(|v| v.len() == 2)
+    .map(|v| v.iter().product::<u32>())
+    .sum()
 }
 
 #[test]
@@ -79,4 +135,22 @@ fn example1() {
 .664.598..
 ";
     assert_eq!(part1(input), 4361);
+}
+
+
+#[test]
+fn example2() {
+    let input = "
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+";
+    assert_eq!(part2(input), 467835);
 }
