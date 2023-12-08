35d4af76cf49f6cb41b14acc10083f751c9a21ce
Mark Day <mday@mac.com>
2023-12-08 14:53:32 -0700

    Day 6, part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -3,7 +3,11 @@ fn main() {
 
     let result1 = part1(input);
     println!("Part 1: {result1}");
-    // assert_eq!(result1, 13);
+    assert_eq!(result1, 633080);
+
+    let result2 = part2(input);
+    println!("Part 2: {result2}");
+    assert_eq!(result2, 20048741);
 }
 
 fn part1(input: &str) -> u32 {
@@ -35,6 +39,45 @@ fn part1(input: &str) -> u32 {
     result
 }
 
+fn part2(input: &str) -> usize {
+    let mut lines = input.lines();
+    let time: usize = lines
+        .next()
+        .expect("one line")
+        .split_once(':')
+        .expect("colon")
+        .1
+        .replace(' ', "")
+        .parse()
+        .expect("valid number");
+    let distance: usize = lines
+        .next()
+        .expect("one line")
+        .split_once(':')
+        .expect("colon")
+        .1
+        .replace(' ', "")
+        .parse()
+        .expect("valid number");
+
+    // Find a win with the smallest possible charge time
+    let mut lower = 0;          // Always a loser
+    let mut upper = time / 2;   // Always a winner
+    while lower+1 < upper {
+        let mid = (lower + upper) / 2;
+        if mid * (time - mid) > distance {
+            upper = mid;
+        } else {
+            lower = mid;
+        }
+    }
+    let shortest = upper;
+
+    // The charge time and move time are symmetric, so the longest charge
+    // time is (time - shortest).
+    (time - shortest) - shortest + 1
+}
+
 #[cfg(test)]
 static EXAMPLE_INPUT: &str = "\
 Time:      7  15   30
@@ -44,4 +87,9 @@ Distance:  9  40  200
 #[test]
 fn test_part1_example() {
     assert_eq!(part1(EXAMPLE_INPUT), 288);
+}
+
+#[test]
+fn test_part2_example() {
+    assert_eq!(part2(EXAMPLE_INPUT), 71503);
 }
