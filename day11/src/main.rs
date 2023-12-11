6ff0fbe3c0d8327b72d491d1dbbca4438be2dbe0
Mark Day <mday@mac.com>
2023-12-11 11:49:05 -0700

    Day 11, part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -7,6 +7,10 @@ fn main() {
     let result1 = part1(input);
     println!("Part 1: {result1}");
     assert_eq!(result1, 9403026);
+
+    let result2 = part2(input);
+    println!("Part 2: {result2}");
+    assert_eq!(result2, 543018317006);
 }
 
 fn part1(input: &str) -> usize {
@@ -44,6 +48,45 @@ fn part1(input: &str) -> usize {
                 + (min_col..max_col)
                     .map(|row| if occupied_cols.contains(&row) { 0 } else { 1 })
                     .sum::<usize>()
+        })
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
+    let mut galaxies = Vec::<(usize, usize)>::new(); // (row, col)
+    let mut occupied_rows = HashSet::<usize>::new();
+    let mut occupied_cols = HashSet::<usize>::new();
+
+    // Parse the input.  Find the location of all of the galaxies.
+    // While we're parsing, also make note of which rows and columns
+    // contain a galaxy.
+    for (row, line) in input.lines().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch == '#' {
+                galaxies.push((row, col));
+                occupied_rows.insert(row);
+                occupied_cols.insert(col);
+            }
+        }
+    }
+
+    // Find the Manhattan distance between each pair of galaxies,
+    // adjusted for empty rows and columns between them.  And sum
+    // those all up.
+    galaxies
+        .iter()
+        .combinations(2)
+        .map(|pair| {
+            let (min_row, max_row) = minmax(pair[0].0, pair[1].0);
+            let (min_col, max_col) = minmax(pair[0].1, pair[1].1);
+
+            max_row - min_row + max_col - min_col
+                + (min_row..max_row)
+                    .map(|row| if occupied_rows.contains(&row) { 0 } else { 1 })
+                    .sum::<usize>() * 999999
+                + (min_col..max_col)
+                    .map(|row| if occupied_cols.contains(&row) { 0 } else { 1 })
+                    .sum::<usize>() * 999999
         })
         .sum()
 }
