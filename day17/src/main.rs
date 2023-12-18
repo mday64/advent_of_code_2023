7a405cbfb2cabe23617810529c6cdea79b0caa17
Mark Day <mday@mac.com>
2023-12-18 15:35:23 -0700

    Day 17, part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -7,6 +7,10 @@ fn main() {
     let result1 = part1(input);
     println!("Part 1: {result1}");
     assert_eq!(result1, 963);
+
+    let result2 = part2(input);
+    println!("Part 2: {result2}");
+    assert_eq!(result2, 1178);
 }
 
 fn part1(input: &str) -> u32 {
@@ -58,6 +62,67 @@ fn part1(input: &str) -> u32 {
     };
     let success = |node: &Node| -> bool {
         (node.row == last_row) && (node.col == last_col)
+    };
+
+    let (_path, cost) = astar(&start, successors, heuristic, success).unwrap();
+    // dbg!(_path);
+    cost
+}
+
+fn part2(input: &str) -> u32 {
+    // Parse the input into a hashmap (which makes boundary detection easier)
+    let mut map: HashMap<(i32, i32), u32> = HashMap::new();
+    for (row, line) in input.lines().enumerate() {
+        for (col, value) in line.chars().map(|c| c.to_digit(10).unwrap()).enumerate() {
+            map.insert((row as i32, col as i32), value);
+        }
+    }
+    let last_row = input.lines().count() as i32 - 1;
+    let last_col = input.lines().next().unwrap().len() as i32 - 1;
+
+    let neighbor_in_direction = |node: &Node, direction: Direction| -> Option<(Node, u32)> {
+        let count = if node.direction == direction {
+            node.count + 1
+        } else {
+            1
+        };
+        if count > 10 {
+            // Can't go straight more than 10 blocks
+            return None;
+        }
+        if node.count > 0 && node.direction != direction && node.count < 4 {
+            // Can't turn too quickly
+            return None;
+        }
+
+        let (row, col) = match direction {
+            Direction::Up => (node.row - 1, node.col),
+            Direction::Down => (node.row + 1, node.col),
+            Direction::Left => (node.row, node.col - 1),
+            Direction::Right => (node.row, node.col + 1),
+        };
+
+        map.get(&(row, col)).map(|&cost| (Node{row, col, direction, count}, cost))
+    };
+    let start = Node::new(0, 0, Direction::Right, 0);
+    let successors = |node: &Node| -> Vec<(Node, u32)> {
+        let mut result = vec![];
+        if let Some(neighbor) = neighbor_in_direction(node, node.direction) {
+            result.push(neighbor);
+        }
+        if let Some(neighbor) = neighbor_in_direction(node, node.direction.turn_left()) {
+            result.push(neighbor);
+        }
+        if let Some(neighbor) = neighbor_in_direction(node, node.direction.turn_right()) {
+            result.push(neighbor);
+        }
+        result
+    };
+    let heuristic = |node: &Node| -> u32 {
+        ((last_row - node.row) + (last_col - node.col)) as u32
+    };
+    let success = |node: &Node| -> bool {
+        (node.row == last_row) && (node.col == last_col) && (node.count >= 4)
     };
 
     let (_path, cost) = astar(&start, successors, heuristic, success).unwrap();
@@ -126,4 +191,9 @@ static EXAMPLE1: &str = "\
 #[test]
 fn test_part1() {
     assert_eq!(part1(EXAMPLE1), 102);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(EXAMPLE1), 94);
 }
