d68e4d247b92df380c924df722042a3744fe3d01
Mark Day <mday@mac.com>
2023-12-21 15:59:54 -0700

    Day 21, part 2: not correct yet

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -6,6 +6,10 @@ fn main() {
     let result1 = part1(input, 64);
     println!("Part 1: {result1}");
     assert_eq!(result1, 3687);
+
+    let result2 = part2(input, 26_501_365);
+    println!("Part 2: {result2}");
+    assert_eq!(result2, 3687);
 }
 
 fn part1(input: &str, steps: u32) -> usize {
@@ -47,8 +51,51 @@ fn part1(input: &str, steps: u32) -> usize {
     reachable.len()
 }
 
+fn part2(input: &str, steps: u32) -> usize {
+    // Note: the input grid repeats infinitely in all directions.
+    // Note: the outer edges of the grid are never rocks.
+    let mut start = None;
+    let mut rocks = HashSet::new();
+    let mut reachable = HashMap::new();
+
+    // Parse the input, keeping track of the locations of rocks and
+    // the starting point.
+    for (row, line) in input.lines().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch == 'S' {
+                start = Some((row as isize, col as isize));
+            } else if ch == '#' {
+                rocks.insert((row as isize, col as isize));
+            }
+        }
+    }
+    let start = start.expect("should find the starting point");
+    let num_rows = input.lines().count() as isize;
+    let num_cols = input.lines().next().unwrap().len() as isize;
+
+    reachable.insert(start, 1);
+
+    for _step in 0..steps {
+        // Find any points reachable from the previous step
+        let mut next_generation = HashMap::new();
+
+        for ((row, col), count) in reachable {
+            for (r,c) in [(row-1, col), (row, col-1), (row, col+1), (row+1, col)] {
+                let r = (r + num_rows) % num_rows;
+                let c = (c + num_cols) % num_cols;
+                if !rocks.contains(&(r,c)) {
+                    *next_generation.entry((r, c)).or_default() += count;
+                }
+            }
+        }
+        reachable = next_generation;
+    }
+
+    reachable.values().sum()
+}
+
 #[cfg(test)]
-static EXAMPLE1: &str = "
+static EXAMPLE1: &str = "\
 ...........
 .....###.#.
 .###.##..#.
@@ -65,4 +112,39 @@ static EXAMPLE1: &str = "
 #[test]
 fn test_part1() {
     assert_eq!(part1(EXAMPLE1, 6), 16);
+}
+
+#[test]
+fn test_part2_6() {
+    assert_eq!(part2(EXAMPLE1, 6), 16);
+}
+
+#[test]
+fn test_part2_10() {
+    assert_eq!(part2(EXAMPLE1, 10), 50);
+}
+
+#[test]
+fn test_part2_50() {
+    assert_eq!(part2(EXAMPLE1, 50), 1594);
+}
+
+#[test]
+fn test_part2_100() {
+    assert_eq!(part2(EXAMPLE1, 100), 6536);
+}
+
+#[test]
+fn test_part2_500() {
+    assert_eq!(part2(EXAMPLE1, 500), 167004);
+}
+
+#[test]
+fn test_part2_1000() {
+    assert_eq!(part2(EXAMPLE1, 1000), 668697);
+}
+
+#[test]
+fn test_part2_5000() {
+    assert_eq!(part2(EXAMPLE1, 5000), 16733044);
 }
