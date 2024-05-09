773ea16f63e49ab7517ce51b64708b6ceafa7ed7
Mark Day <mday@mac.com>
2024-05-09 16:03:52 -0600

    Day 20, part 2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::{VecDeque, HashMap};
+use num::Integer;
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -9,7 +10,7 @@ fn main() {
 
     let result2 = part2(input);
     println!("Part 2: {result2}");
-    assert_eq!(result2, 841763884);
+    assert_eq!(result2, 246006621493687);
 }
 
 fn part1(input: &str) -> u32 {
@@ -124,7 +125,16 @@ fn part1(input: &str) -> u32 {
 // sent to "rx".  It's probably some annoying counters plus multiplication
 // (multiple counters have to get to their magic value at the same time).
 //
-fn part2(input: &str) -> u32 {
+// So, that final node, "rx" has one input: a conjunction (NAND) node.
+// That conjunction node has multiple inputs (4 in my input).  It will
+// send the desired low pulse to "rx" when all of its inputs are
+// simultaneously high.  I'm assuming that those inputs go high every
+// "N" button presses (with different "N" for each input; those values
+// are probably relatively prime).  The solution is to find the number
+// of button pushes to make each of those inputs go high, and then
+// calculate the lowest common multiple for the answer.
+//
+fn part2(input: &str) -> u64 {
     // Flip-flop state: map name to on/off
     let mut flip_flops = HashMap::<&str, bool>::new();
     // Conjunction memory: map name to (sender -> last pulse)
@@ -169,24 +179,40 @@ fn part2(input: &str) -> u32 {
         }
     }
 
-    let mut queue = VecDeque::<Pulse>::new();
+    // Figure out the name of the conjunction node that sends to "rx"
+    let mut conjunction_name = None;
+    for (sender, receivers) in destinations.iter() {
+        if receivers.first() == Some(&"rx") {
+            conjunction_name = Some(sender);
+            break;
+        }
+    }
+    let conjunction_name = *conjunction_name.unwrap();
 
-    let mut done = false;
+    // Figure out the names of the nodes that send to the conjunction node.
+    // Create a HashMap to keep track of the cycle lengths.
+    // Note: I'm assuming that all of the cycles start with the very first
+    // button press, and there are no initial states prior to the first cycle.
+    let mut cycle_lengths: HashMap<&str, Option<u64>> = HashMap::new();
+    for (sender, receivers) in destinations.iter() {
+        if receivers.contains(&conjunction_name) {
+            cycle_lengths.insert(sender, None);
+        }
+    }
+
+    // Start pressing the button until we have determined all of the cycle lengths.
+    let mut queue = VecDeque::<Pulse>::new();
     let mut presses = 0;
 
-    while !done {
-        // Button sends a low pulse to "broadcaster"
+    loop {
         presses += 1;
+
+        // Button sends a low pulse to "broadcaster"
         queue.push_back(Pulse{sender: "button", receiver: "broadcaster", high: false});
 
         // Process all of the pulses that result from the button push
         while let Some(Pulse{sender, receiver, high}) = queue.pop_front() {
             // Process this pulse
-
-            if receiver == "rx" && !high {
-                done = true;
-            }
-
             match module_kind.get(receiver) {
                 Some(ModuleKind::FlipFlop) => {
                     if !high {
@@ -212,6 +238,13 @@ fn part2(input: &str) -> u32 {
                     for destination in destinations.get(receiver).unwrap() {
                         queue.push_back(Pulse{sender: receiver, receiver: destination, high: send_high});
                     }
+
+                    // If the receiver was the last conjunction node, and the
+                    // pulse was high, then set this sender's cycle length
+                    // (if it was not yet set).
+                    if receiver == conjunction_name && high {
+                        cycle_lengths.insert(sender, Some(presses));
+                    }
                 }
                 Some(ModuleKind::Generic) => {
                     for destination in destinations.get(receiver).unwrap() {
@@ -223,9 +256,19 @@ fn part2(input: &str) -> u32 {
                 }
             }
         }
+
+        // Have we determined all of the cycle lengths yet?
+        if cycle_lengths.values().all(Option::is_some) {
+            break;
+        }
     }
 
-    presses
+    // Calculate LCM of the various cycle lengths
+    let mut result = 1;
+    for v in cycle_lengths.values() {
+        result = result.lcm(&v.unwrap());
+    }
+    result
 }
 
 #[derive(Debug, Clone, Copy)]
