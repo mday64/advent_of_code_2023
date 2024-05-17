0d1cfffcc6b7a6ab8c778d6f1a30e1dfad32a8de
Mark Day <mday@mac.com>
2024-05-17 15:50:04 -0600

    Initial work on part 2
    * Various asserts and simplifying assumptions
    * Calculate number of steps to all locations from any edge or corner

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -9,7 +9,7 @@ fn main() {
 
     let result2 = part2(input, 26_501_365);
     println!("Part 2: {result2}");
-    assert_eq!(result2, 3687);
+    assert_eq!(result2, 0);
 }
 
 fn part1(input: &str, steps: u32) -> usize {
@@ -51,12 +51,55 @@ fn part1(input: &str, steps: u32) -> usize {
     reachable.len()
 }
 
+//
+// The input repeats infinitely in all four directions.  We have to
+// calculate how many of the open (non-rock) locations can be reached
+// in the given number of steps.  For ease of discussion, I'm going
+// to call each copy of the input a "section."
+//
+// Because steps are only up/down/left/right, and no diagonals, any
+// given location can only be an odd number of steps or an even number
+// of steps, but not both.  In fact, this even/odd-ness is the same
+// as the Manhattan distance to that location.  That means the problem
+// becomes finding the minimum number of steps at which a location
+// becomes reachable.  Any location reachable with the input number
+// of steps or less, and the same even/odd-ness, will be reachable
+// in the last step (at worst, you just keep stepping back and forth
+// to an adjacent open space).
+//
+// In our input, there is a direct, unobstructed path from the starting
+// location to each edge, traveling in just one direction (all up, all down,
+// etc.).  The edges are also unobstructed.
+//
+// I believe that the rocks are sparse enough that no interior location is
+// a further path distance than that section's furthest corner.  Therefore,
+// if the furthest corner has a Manhattan distance less than or equal to
+// the input number of steps, then all empty (non-rock) locations with the
+// correct odd/even-ness are reachable.  We can pre-compute these numbers
+// (one for odd, one for even) in advance.
+//
+// For sections where the nearest edge/corner is within the given number
+// of steps, but the furthest corner is not, then we have to explicitly
+// count the number of steps from the closest location.
+//
+// In my input, the number of rows and columns are odd.  This means that
+// the odd/even-ness of corresponding locations alternates every time
+// you step through a section.  I think all sections on a diagonal will
+// have the same odd/even-ness.
+//
+// Recall that for Manhattan distance, equal distance looks like a diamond
+// (a square rotated by 45 degrees).  So all sections completely inside
+// the given number of steps will form a filled-in diamond.  It's not quite
+// as simple as caluclating the number of sections times the number of
+// reachable locations because of the odd/even-ness.  So there will be two
+// numbers of reachable locations, and we have to figure out how many sections
+// of each to add.
+//
 fn part2(input: &str, steps: u32) -> usize {
     // Note: the input grid repeats infinitely in all directions.
     // Note: the outer edges of the grid are never rocks.
     let mut start = None;
     let mut rocks = HashSet::new();
-    let mut reachable = HashMap::new();
 
     // Parse the input, keeping track of the locations of rocks and
     // the starting point.
@@ -73,25 +116,101 @@ fn part2(input: &str, steps: u32) -> usize {
     let num_rows = input.lines().count() as isize;
     let num_cols = input.lines().next().unwrap().len() as isize;
 
-    reachable.insert(start, 1);
+    // The starting position should be exactly in the middle of the input.
+    assert!(num_rows & 1 == 1);
+    assert!(num_cols & 1 == 1);
+    assert!(num_rows == start.0 * 2 + 1);
+    assert!(num_cols == start.1 * 2 + 1);
 
-    for _step in 0..steps {
-        // Find any points reachable from the previous step
-        let mut next_generation = HashMap::new();
+    // Simplifying assumption: the input is square, so distances are
+    // the same in all directions.
+    assert_eq!(num_rows, num_cols);
+    assert_eq!(start.0, start.1);
+    let start = start.0;
+    let dimension = num_rows;
 
-        for ((row, col), count) in reachable {
-            for (r,c) in [(row-1, col), (row, col-1), (row, col+1), (row+1, col)] {
-                let r = (r + num_rows) % num_rows;
-                let c = (c + num_cols) % num_cols;
-                if !rocks.contains(&(r,c)) {
-                    *next_generation.entry((r, c)).or_default() += count;
-                }
-            }
-        }
-        reachable = next_generation;
+    // There should be no rocks from the starting point to any edge.
+    // There should be no rocks on the outer edges.
+    for r in 0..num_rows {
+        assert!(!rocks.contains(&(r, start)));
+        assert!(!rocks.contains(&(r, 0)));
+        assert!(!rocks.contains(&(r, num_cols - 1)));
+    }
+    for c in 0..num_cols {
+        assert!(!rocks.contains(&(start, c)));
+        assert!(!rocks.contains(&(0, c)));
+        assert!(!rocks.contains(&(num_rows - 1, c)));
     }
 
-    reachable.values().sum()
+    // Precompute the number of steps from any corner or edge middle to
+    // any non-rock.  The furthest corner should have the maximum distance.
+    // The key of the outer map is (r, c), where r and c are the signum of
+    // the number of sections relative to the starting section (r = rows,
+    // c = columns).  For example, (0, -1) would be the same row as the
+    // starting section, and to the left.  The origin, the nearest point
+    // to the starting point, would be the middle of the right edge.
+    // The value is a vector of the number of steps to every non-rock.
+    let mut reachable_from = HashMap::<(isize, isize), Vec<usize>>::new();
+    reachable_from.insert((0, 1), shortest_paths(start, 0, dimension, &rocks));
+    reachable_from.insert((0, -1), shortest_paths(start, dimension-1, dimension, &rocks));
+    reachable_from.insert((1, 0), shortest_paths(0, start, dimension, &rocks));
+    reachable_from.insert((-1, 0), shortest_paths(dimension-1, start, dimension, &rocks));
+    reachable_from.insert((-1, -1), shortest_paths(dimension-1, dimension-1, dimension, &rocks));
+    reachable_from.insert((-1, 1), shortest_paths(dimension-1, 0, dimension, &rocks));
+    reachable_from.insert((1, -1), shortest_paths(0, dimension-1, dimension, &rocks));
+    reachable_from.insert((1, 1), shortest_paths(0, 0, dimension, &rocks));
+
+    // Do we also need number of steps from start (middle of section) to
+    // the remainder of the section?  Or do we just need to know odd/even?
+    // The distance from an edge to opposite corner (either one!) is
+    //      start + section_size - 1
+    // The distance from one corner to opposite corner is
+    //      2 * (section_size - 1)
+    // The distance from starting location to non-diagonal adjacent section edge is
+    //      start + 1
+    // The distance from starting location to diagonally adjacent section corner is
+    //      2 * (start + 1)
+
+    // Calculate the range of sections that are entirely reachable in the given
+    // number of steps.  Multiply by the number of reachable locations per section.
+
+    // For each section that is partially reachable, count how many locations
+    // are reachable from the nearest edge or corner in the remaining number
+    // of steps.
+
+    0
+}
+
+fn shortest_paths(row: isize, col: isize, dimension: isize, rocks: &HashSet<(isize, isize)>) -> Vec<usize> {
+    let mut result = HashMap::<(isize, isize), usize>::new();
+    let mut frontier = VecDeque::<(isize, isize)>::new();
+    result.insert((row, col), 0);
+    frontier.push_back((row, col));
+
+    while let Some((r, c)) = frontier.pop_front() {
+        let dist = *result.get(&(r, c)).unwrap();
+        for (r, c) in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)] {
+            if r >= 0 && r < dimension && c >= 0 && c < dimension
+            && !result.contains_key(&(r, c)) && !rocks.contains(&(r, c)) {
+                result.insert((r, c), dist+1);
+                frontier.push_back((r, c));
+            }
+        }
+    }
+
+    // for r in 0..dimension {
+    //     for c in 0..dimension {
+    //         match result.get(&(r,c)) {
+    //             None => print!("    #"),
+    //             Some(dist) => print!("{:5?}", dist)
+    //         }
+    //     }
+    //     println!();
+    // }
+
+    let result: Vec<_> = result.values().cloned().collect();
+    println!("{}", result.iter().max().unwrap());
+    result
 }
 
 #[cfg(test)]
